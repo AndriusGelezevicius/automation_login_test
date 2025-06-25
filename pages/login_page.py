f1a61abbf38a489248e27d3ba657fedd5c6905d8
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-06-25 22:12:56 +0300

    created login_page.py and functions

---
+++ pages/login_page.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_field = (By.ID, "username")
+        self.password_field = (By.ID, "password")
+        self.submit_button = (By.ID, "submit")
+
+    def enter_username(self, username):
+        self.driver.find_element(*self.username_field).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.password_field).send_keys(password)
+
+    def click_submit(self, submit):
+        self.driver.find_element(*self.submit_button).click()
