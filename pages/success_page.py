675df7173203f232c42950f8f32674b606eaed7e
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-06-25 22:28:28 +0300

    created success_page.py and functions

---
+++ pages/success_page.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+class SuccessPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.message_header = (By.TAG_NAME, "h1")
+        self.logout_button = (By.LINK_TEXT, "Log out")
+
+    def get_message_header(self):
+        return self.driver.find_element(*self.message_header).text
+
+    def is_logout_button_displayed(self):
+        return self.driver.find_element(*self.logout_button).is_displayed()
+
