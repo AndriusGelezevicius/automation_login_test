9c439eb9c2a3ec762325fac9a7d79422987b18ec
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-06-29 20:08:08 +0300

    created test_negative_password.py and function

--- tests/test_negative_password.py
+++ tests/test_negative_password.py
@@ -3,9 +3,9 @@ from pages.login_page import LoginPage
 def test_invalid_username(driver):
     login_page = LoginPage(driver)
     login_page.open("https://practicetestautomation.com/practice-test-login/")
-    login_page.enter_username("incorrectUser")
-    login_page.enter_password("Password123")
+    login_page.enter_username("student")
+    login_page.enter_password("incorrectPassword")
     login_page.click_submit()
 
     assert login_page.is_error_message_displayed()
-    assert login_page.get_error_message() == "Your username is invalid!"
+    assert login_page.get_error_message() == "Your password is invalid!"
